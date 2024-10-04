fa3d82104e0c1292fd495e99aaeeba6f078643c3
Samanvya Tripathi <samanu09@gmail.com>
2024-10-04 17:22:37 -0400

    Add trial details and write to json

--- web_scraper.py
+++ web_scraper.py
@@ -2,8 +2,11 @@ import asyncio
 import json
 from crawl4ai import AsyncWebCrawler
 from crawl4ai.extraction_strategy import JsonCssExtractionStrategy
+from collections import defaultdict
+from tenacity import retry, stop_after_attempt, wait_exponential
+import os
 
-async def extract_nct_ids(page_number=1):
+async def extract_nct_ids(crawler, trials_per_page=25, page_number=1):
     # Define the extraction schema
     # ctg-search-results-table p-table div#pn_id_2 table tr.ng-star-inserted 
     schema = {
@@ -17,18 +20,11 @@ async def extract_nct_ids(page_number=1):
             }
         ]
     }
-    # js_code = """
-    #         await new Promise(resolve => {
-    #             window.onload = function() {
-    #                 console.log('Page has fully loaded.');
-    #                 resolve();
-    #             };
-    #         });
-    #         """
+
     js_code = """
     (async () => {
         window.scrollTo(0, document.body.scrollHeight);
-        await new Promise(resolve => setTimeout(resolve, 2000));  // Wait for 5 seconds
+        await new Promise(resolve => setTimeout(resolve, 2000));  // Wait for 2 seconds
     })();
     """
 
@@ -36,46 +32,82 @@ async def extract_nct_ids(page_number=1):
     extraction_strategy = JsonCssExtractionStrategy(schema, verbose=True)
 
     # Use the AsyncWebCrawler with the extraction strategy
-    async with AsyncWebCrawler(verbose=True) as crawler:
-        result = await crawler.arun(
-            url=f"https://clinicaltrials.gov/search?viewType=Table&aggFilters=status:rec&limit=100&page={page_number}",
-            extraction_strategy=extraction_strategy,
-            js_code=js_code,
-            bypass_cache=True
-        )
+    result = await crawler.arun(
+        url=f"https://clinicaltrials.gov/search?viewType=Table&aggFilters=status:rec&limit={trials_per_page}&page={page_number}",
+        extraction_strategy=extraction_strategy,
+        js_code=js_code,
+        bypass_cache=True
+    )
 
-        assert result.success, "Failed to crawl the page"
-        # Parse the extracted content
-        extracted_data = json.loads(result.extracted_content)
-        # Parse JSON and go to the extracted page
-        id_set = set(id['nct_id'] for id in extracted_data)
-        print(id_set)
-        trial_page_dict = {}
-        for nct_id in id_set:
-            # nct_id = row['nct_id']
-            print("Processing: ",nct_id," #################")
-            trial_details_per_id = await crawler.arun(
-            url=f"https://clinicaltrials.gov/study/{nct_id}",
-            bypass_cache=True
-            )
-            assert result.success, "Failed to crawl the page"
-            print(trial_details_per_id.markdown)
-            trial_page_dict[nct_id] = trial_details_per_id.markdown
-            print("Finished adding to dict ################")
-        
+    assert result.success, "Failed to crawl the page"
+    # Parse the extracted content
+    extracted_data = json.loads(result.extracted_content)
+    # Parse JSON and go to the extracted page
+    id_set = set(id['nct_id'] for id in extracted_data)
+    return id_set
 
+@retry(stop=stop_after_attempt(3), wait=wait_exponential(multiplier=1, min=4, max=10))
+async def get_trial_details_by_id(crawler, trial_id):
+    
+    trial_page_schema = {
+        "name": "Clinical Trial Data",
+        "baseSelector": "div.content",
+        "fields": [
+            {
+                "name": "Study Overview",
+                "selector": "div#study-overview",
+                "type": "text"
+            },
+            {
+                "name": "Participation Criteria",
+                "selector": "#participation-criteria > ctg-participation-criteria:nth-child(2)",
+                "type": "text"
+            }
+        ]
+    }
+
+    extraction_strategy = JsonCssExtractionStrategy(trial_page_schema, verbose=True)
+    print("Processing: ",trial_id," #################")
+    
+    trial_details_per_id = await crawler.arun(
+                            url=f"https://clinicaltrials.gov/study/{trial_id}",
+                            extraction_strategy=extraction_strategy,
+                            bypass_cache=True
+                            )
+    assert trial_details_per_id.success, "Failed to crawl the page"
+    print(trial_details_per_id.extracted_content)
+    # print(json.dumps(trial_details_per_id.extracted_data, indent=2))
+    return json.loads(trial_details_per_id.extracted_content)[0]
+
+def write_to_json(filename, data):
+    save_dir = './clinical_trial_data'
+    os.makedirs(save_dir, exist_ok=True)
+    file_save_path = os.path.join(save_dir, filename)
+
+    with open(file_save_path, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+    print(f"Written file {file_save_path}")
 
 async def main():
+    max_pages = 15
+    trials_per_page = 50
+    async with AsyncWebCrawler(verbose=True) as crawler:
+        page_number = 1
+        
+        while page_number<max_pages:
+            trial_id_data_dict = defaultdict()
+            # Get the currently recruiting Trial IDs
+            trial_ids_set = await extract_nct_ids(crawler, 
+                                                  trials_per_page,
+                                                  page_number=page_number)
 
-    await extract_nct_ids()
-    # page_number = 1
-    # all_trials = []
-    # while True:
-    #     trial_ids = extract_nct_ids(page_number=page_number)
-    #     if trial_ids:
-    #         all_trials.append(trial_ids)
-    #         page_number += 1
-    #     else:
-    #         break
+            for id in trial_ids_set:
+                trial_page_details = await get_trial_details_by_id(crawler, id)
+                trial_id_data_dict[id] = {}
+                trial_id_data_dict[id]["Study Overview"] = trial_page_details["Study Overview"]
+                trial_id_data_dict[id]["Participation Criteria"] = trial_page_details["Participation Criteria"]
+            
+            write_to_json(f'clinical_trial_data_page{page_number}.json', trial_id_data_dict)
+            page_number+=1
 
 asyncio.run(main())
